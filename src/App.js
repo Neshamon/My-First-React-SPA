e8906e8bcc203e5ed3bb191cd36d81a21c30ef7c
John Matthews <neshamon@protonmail.com>
2022-06-25 17:34:07 -0500

    Render components to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from "./Components/Main"
+import Navbar from "./Components/Navbar"
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Navbar />
+        <Main />
       </header>
     </div>
   );
