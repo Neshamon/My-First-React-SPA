8077652bd2ac4495933985371eee2b99624b5ba1
John Matthews <neshamon@protonmail.com>
2022-06-25 22:18:35 -0500

    Add class to main h1

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Main() {
   return(
     <main>
-      <h1>This is my Main Content</h1>
+      <h1 className = "main-title">This is my Main Content</h1>
     </main>
   )
 }
