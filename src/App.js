0552759e43796630c2b6ebf7d06c91c51a1f5825
John Matthews <neshamon@protonmail.com>
2022-06-25 21:05:51 -0500

    Relocate logo svg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from "./Components/Main"
 import Navbar from "./Components/Navbar"
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Navbar />
         <Main />
       </header>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,10 @@
+import logo from '../logo.svg';
 import React from "react";
 
 export default function Navbar() {
   return(
     <nav>
+      <img src={logo} className="App-logo" alt="logo" />
       <h2 className = "">React Facts</h2>
     </nav>
   )
