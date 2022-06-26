5b93668bbc3a26efa94cb22ed7950c2d6270c3c3
John Matthews <neshamon@protonmail.com>
2022-06-25 22:19:18 -0500

    Create unordered list

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,6 +4,13 @@ export default function Main() {
   return(
     <main>
       <h1 className = "main-title">This is my Main Content</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </main>
   )
 }
