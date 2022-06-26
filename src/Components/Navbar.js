191aae32ef1cefe2dbad9c236d2b55b7a9298994
John Matthews <neshamon@protonmail.com>
2022-06-25 21:22:31 -0500

    Add header tags to Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,11 @@ import React from "react";
 
 export default function Navbar() {
   return(
-    <nav>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2 className = "nav-title">React Facts</h2>
-    </nav>
+    <header>
+      <nav>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2 className = "nav-title">React Facts</h2>
+        </nav>
+    </header>
   )
 }
